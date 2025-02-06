b60bb858b8e2424e0217fcbd1fcacc85406dabae
Aditya Rao <adityarao2005@gmail.com>
2025-02-05 21:29:10 -0500

    filling out a lot of the first page

--- ieee-website-app/src/state/user-slice.ts
+++ ieee-website-app/src/state/user-slice.ts
@@ -1,25 +1,22 @@
-// import { User } from "@/models/data/user";
-// import { createSlice } from "@reduxjs/toolkit";
-// import type { PayloadAction } from "@reduxjs/toolkit";
+import { UserState } from "@/models/data/user";
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-// export const initialState: User = {
-//     id: "",
-//     name: "",
-//     email: "",
-// }
+export const initialState: UserState = {
+}
 
-// const userSlice = createSlice({
-//     name: "user",
-//     initialState,
-//     reducers: {
-//         setUser(state, action: PayloadAction<User>) {
-//             return action.payload;
-//         },
-//         clearUser(state) {
-//             return initialState;
-//         }
-//     }
-// });
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUser(state, action: PayloadAction<UserState>) {
+            return action.payload;
+        },
+        clearUser(state) {
+            return initialState;
+        }
+    }
+});
 
-// export const { setUser, clearUser } = userSlice.actions;
-// export default userSlice.reducer;
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
