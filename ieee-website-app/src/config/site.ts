c8ffd5cbbe82cc6651ee313892937ba654ffa1a6
Aditya Rao <adityarao2005@gmail.com>
2025-02-25 13:24:41 -0500

    working wiht the site

--- ieee-website-app/src/config/site.ts
+++ ieee-website-app/src/config/site.ts
@@ -1,66 +1,74 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "About Us",
-      href: "/#about",
-    },
-    {
-      label: "Chapters",
-      href: "/chapters",
-    },
-    {
-      label: "Services",
-      href: "/services",
-    },
-    {
-      label: "Contact Us",
-      href: "/#contact",
-    },
-    {
-      label: "Login",
-      href: "/login",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
-    },
-  ],
+    name: "Next.js + NextUI",
+    description: "Make beautiful websites regardless of your design experience.",
+    navItems: [
+        {
+            label: "Home",
+            href: "/",
+        },
+        {
+            label: "About Us",
+            href: "/#about",
+        },
+        {
+            label: "Chapters",
+            href: "/chapters",
+        },
+        {
+            label: "Services",
+            href: "/services",
+        },
+        {
+            label: "Contact Us",
+            href: "/#contact",
+        },
+        {
+            label: "Faculty",
+            href: "/faculty",
+        },
+        {
+            label: "News",
+            href: "/news",
+        },
+        {
+            label: "Login",
+            href: "/login",
+        },
+    ],
+    navMenuItems: [
+        {
+            label: "Profile",
+            href: "/profile",
+        },
+        {
+            label: "Dashboard",
+            href: "/dashboard",
+        },
+        {
+            label: "Projects",
+            href: "/projects",
+        },
+        {
+            label: "Team",
+            href: "/team",
+        },
+        {
+            label: "Calendar",
+            href: "/calendar",
+        },
+        {
+            label: "Settings",
+            href: "/settings",
+        },
+        {
+            label: "Help & Feedback",
+            href: "/help-feedback",
+        },
+        {
+            label: "Logout",
+            href: "/logout",
+        },
+    ],
 };
