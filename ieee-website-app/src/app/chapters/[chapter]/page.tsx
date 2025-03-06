a214491f16450a6b21b49cd68a4ca5e77b45e59b
adityarao2005 <adityarao2005@gmail.com>
2025-03-05 22:45:53 -0500

    fixed some announcement stuff

--- ieee-website-app/src/app/chapters/[chapter]/page.tsx
+++ ieee-website-app/src/app/chapters/[chapter]/page.tsx
@@ -1,17 +1,49 @@
 import { ChapterInfo, getChapterInfoByRoute } from "@/config/site.chapters";
 import { getAnnouncementsByChapter } from "@/models/api/announcements";
+import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
+import { Image } from "@heroui/image";
+import { Divider } from "@heroui/divider";
+import { Link } from "@heroui/link";
+import { Announcements } from "@prisma/client";
 import { notFound } from "next/navigation";
 
-async function AnnouncementsComponent({ chapter }: { chapter: ChapterInfo }) {
-    const announcements = await getAnnouncementsByChapter(chapter);
-
+function Announcement({ announcement }: { announcement: Announcements }) {
 
     return (
-        <section className="flex flex-col gap-4">
-            {announcements.map((announcement) => <div>
-                <div>{announcement.title}</div>
-                <div>{announcement.descriptionContent}</div>
-            </div>)}
+        <Card className="max-w-[400px]">
+            <CardHeader className="flex gap-3">
+                <Image
+                    alt="heroui logo"
+                    height={40}
+                    radius="sm"
+                    src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+                    width={40}
+                />
+                <div className="flex flex-col">
+                    <p className="text-md">{announcement.title}</p>
+                    <p className="text-small text-default-500">{announcement.authorId || "user"}</p>
+                </div>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+                <p>{announcement.descriptionContent}</p>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+                <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
+                    Visit source code on GitHub.
+                </Link>
+            </CardFooter>
+        </Card>
+    );
+}
+
+async function AnnouncementsContent({ chapter }: { chapter: ChapterInfo }) {
+    const announcements = await getAnnouncementsByChapter(chapter);
+
+    return (
+        <section className="flex flex-wrap gap-4">
+            {announcements.map((announcement) => <Announcement key={announcement.id} announcement={announcement} />)}
         </section>
     );
 }
@@ -42,7 +74,7 @@ export default async function ChapterPage({
 
             <div>{chapter.description}</div>
 
-            <AnnouncementsComponent chapter={chapter} />
+            <AnnouncementsContent chapter={chapter} />
         </section>
     );
 }
