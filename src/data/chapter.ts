abb6e56653f6029f3a44e6835c47723dbe55ef87
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2026-09-08 09:04:17 -0400

    Revise chapter descriptions for IEEE societies
    Updated chapter descriptions to reflect new focus areas and activities.

--- src/data/chapter.ts
+++ src/data/chapter.ts
@@ -11,14 +11,14 @@ export const chapters: Chapter[] = [
         name: "IEEE Computer Society",
         title: "Computer Chapter",
         description:
-            "The Computer Chapter is all about turning electronics into real-world solutions. Its focus is on embedded software and PCB circuit design. Whether it's crafting with Arduino, innovating with Raspberry Pi, or applying tech across industries, IEEE Computer Society is at the forefront of making electronics work smarter. Members gain hands-on experience through projects that directly apply to industry.",
+            "The Computer Chapter develops software and hardware technical workshops, providing opportunities for its members to learn and develop new skills, including working with Arduinos, Raspberry Pis, Python, JavaScript, C, APIs, Computer Vision, Web Development and more!",
         icon: "💻",
     },
     {
         name: "Power and Energy Society",
         title: "PES Chapter",
         description:
-            "The PES Chapter focuses on power electronics, with topics like energy transmission and conversion. Members explore power generation, electrical infrastructure, renewable energy, and related fields such as radio frequency (RF), control systems, and analog system design through technical discussions and hands-on learning.",
+            "The Power and Energy Society is a hub for students interested in the bulk electric system, energy generation, bulk equipment, power electronics, etc. We organize workshops aimed towards these interests and hope to network and create a social community!",
         icon: "⚡",
     },
     {
